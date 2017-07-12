183a7d834a2380e2c9c3fa0c05f3fc56eaa4c02a
TexasTemas12 <thomastx12@gmail.com>
2017-07-12 17:27:31 -0500

    first turnin

---
+++ main.js
@@ -0,0 +1,27 @@
+var calcequation = ""
+// sets the name of .calcfield to calcfield
+var calcfield = document.querySelector(".calcfield")
+
+calcfield.textContent = calcequation;
+
+// sets the name of all buttons with class .calcbutt to button
+var buttonList = document.querySelectorAll(".calcbutt")
+
+// adds a listener to each button that waits for a click
+for (i = 0; i < buttonList.length; i++) {
+  buttonList[i].addEventListener('click', buttonPress)
+}
+
+function buttonPress(event) {
+  if (event.target.textContent == 'c') {
+    calcequation = ""
+    calcfield.textContent = calcequation
+  } else if (event.target.textContent == '=') {
+    calcequation = eval(calcequation)
+    calcfield.textContent = calcequation
+  } else {
+    calcequation = calcequation + event.target.textContent
+    calcfield.textContent = calcequation;
+  }
+  return event;
+}
